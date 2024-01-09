f77a3c24558d393cbe07dd04c61c7c61e708c0c3
Deepika <119730634+Deepika22P31A0517@users.noreply.github.com>
2024-01-09 21:23:19 +0530

    import java.util.*; public class Exam {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int t,n,m,k;         t=sc.nextInt();        for(int i=0;i<t;i++)        {            n=sc.nextInt();            m=sc.nextInt();            k=sc.nextInt();            if((m-k)>=n)            {                System.out.println("YES");            }            else            {                System.out.println("NO");            }        }
            }
        
    }

---
+++ Student_Registration.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Exam
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t,n,m,k;
+        t=sc.nextInt();
+       for(int i=0;i<t;i++)
+       {
+           n=sc.nextInt();
+           m=sc.nextInt();
+           k=sc.nextInt();
+           if((m-k)>=n)
+           {
+               System.out.println("YES");
+           }
+           else
+           {
+               System.out.println("NO");
+           }
+       }
+            
+        }
+    
+}
