17434199d81195cf18b5708564a5350f8c9ca5f7
Deepika <119730634+Deepika22P31A0517@users.noreply.github.com>
2024-01-11 22:58:19 +0530

    import java.util.*; public class Exam {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x,y;         x=sc.nextInt();         y=sc.nextInt();          if(x!=0&&x%2==0)     {         System.out.println("YES");     }     else if(x==0&&y%2==0)     {         System.out.println("YES");     }     else     {         System.out.println("NO");     }     } }

---
+++ Can_share_equally?.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Exam
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        x=sc.nextInt();
+        y=sc.nextInt();
+         if(x!=0&&x%2==0)
+    {
+        System.out.println("YES");
+    }
+    else if(x==0&&y%2==0)
+    {
+        System.out.println("YES");
+    }
+    else
+    {
+        System.out.println("NO");
+    }
+    }
+}
