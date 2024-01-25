902eb5902e27ccd5c81d3428ffe777cb81e913c6
Deepika <119730634+Deepika22P31A0517@users.noreply.github.com>
2024-01-25 19:39:50 +0530

    import java.util.*; public class Exam {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String ch=sc.nextLine();         int size=ch.length();         System.out.println(size);     } }

---
+++ String_Length.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Exam
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String ch=sc.nextLine();
+        int size=ch.length();
+        System.out.println(size);
+    }
+}
